effb187db665016e63acca8433c2b93e921da8b0
jlmoncay <jorgeluismoncayopaz1996@gmail.com>
2024-01-05 12:08:56 -0500

    Correciones finales de los botones Perfil y Suscripción. Además, se agregó un botón Animales para que el ususuario regrese a la pantalla principal cuando desee.

--- frontend-mobile-main/src/navigators/OptionsDrawer.js
+++ frontend-mobile-main/src/navigators/OptionsDrawer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createDrawerNavigator, DrawerContentScrollView } from '@react-navigation/drawer';
-import { View, Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -50,11 +50,11 @@ export default function OptionsDrawer() {
   );
 } 
  */
-/* export function DrawerNavigation(){ */
+  /* export function DrawerNavigation(){ */
   return (
     <Drawer.Navigator initialRouteName="Home"
-      drawerContent={(props)=> <MenuItems {...props}/>}
-    
+      drawerContent={(props) => <MenuItems {...props} />}
+
     >
 
       <Drawer.Screen name="Animales" component={HomeTab} />
@@ -62,21 +62,21 @@ export default function OptionsDrawer() {
       <Drawer.Screen name="Suscripción" component={Suscripcion} />
       <Drawer.Screen name="Cerrar sesión" component={HomeTab} />
     </Drawer.Navigator>
-);
+  );
 
 }
 
-const MenuItems=({navigation})=>{
+const MenuItems = ({ navigation }) => {
 
   const [user, setUser] = useState([]);
-  
+
   useEffect(() => {
     //const unsubscribe = navigation.addListener('focus', () => {
-      extraerUsuario();
+    extraerUsuario();
     //});
   }, []);
 
-  
+
 
   const logout = async () => {
     try {
@@ -88,8 +88,7 @@ const MenuItems=({navigation})=>{
   };
 
   const perfil = async () => {
-    try {
-      await AsyncStorage.removeItem('session');
+    try {      
       navigation.navigate('Perfil');
     } catch (error) {
       console.log(error);
@@ -97,101 +96,113 @@ const MenuItems=({navigation})=>{
   };
 
   const suscripcion = async () => {
-    try {
-      await AsyncStorage.removeItem('session');
+    try {      
       navigation.navigate('Suscripcion');
     } catch (error) {
       console.log(error);
     }
   };
 
-  const extraerUsuario= async()=>{
+  const animales = async () => {
+    try {      
+      navigation.navigate('Home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const extraerUsuario = async () => {
     const jsonValue = await AsyncStorage.getItem('session');
-    const data = await JSON.parse(jsonValue);    
-    setUser(data);    
+    const data = await JSON.parse(jsonValue);
+    setUser(data);
   }
 
-
-
-
-
-  return(
+  return (
     <DrawerContentScrollView style={styles.container}>
-   
-     <View style={{justifyContent:'center', alignItems:'center'}}>
-      <Image
-          source={require('../assets/img/logo.png') }
+
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Image
+          source={require('../assets/img/logo.png')}
           style={styles.imagen}
-      /> 
-     
-      <Text style={styles.txtUser}>{user.nombres+" "+user.apellidos}</Text>
-      <Text style={styles.btnText}>{user.email}</Text>
-      <ExtraView></ExtraView>
+        />
 
-    </View>
+        <Text style={styles.txtUser}>{user.nombres + " " + user.apellidos}</Text>
+        <Text style={styles.btnText}>{user.email}</Text>
+        <ExtraView></ExtraView>
 
-    <View style={styles.btnContainer}>
-      <Ionicons name="paw" size={15} color={Colors.greenPet} />
-      <TouchableOpacity style={styles.btnContainer} onPress={suscripcion}>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Ionicons name="paw" size={15} color={Colors.greenPet} />
+        <TouchableOpacity style={styles.btnContainer} onPress={animales}>
+          <Text style={styles.btnText}>Animales</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Ionicons name="paw" size={15} color={Colors.greenPet} />
+        <TouchableOpacity style={styles.btnContainer} onPress={suscripcion}>
           <Text style={styles.btnText}>Suscripción</Text>
         </TouchableOpacity>
-    </View>
-    <View style={styles.btnContainer}>
-      <Ionicons name="person" size={15} color={Colors.greenPet} />
-      <TouchableOpacity style={styles.btnContainer} onPress={perfil}>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Ionicons name="person" size={15} color={Colors.greenPet} />
+        <TouchableOpacity style={styles.btnContainer} onPress={perfil}>
           <Text style={styles.btnText}>Perfil</Text>
         </TouchableOpacity>
-    </View>
-    <View style={styles.btnContainer}>
-      <Ionicons name="power" size={15} color={Colors.greenPet} />
-      <TouchableOpacity style={styles.btnContainer} 
-      onPress={logout}
-      >
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Ionicons name="power" size={15} color={Colors.greenPet} />
+        <TouchableOpacity style={styles.btnContainer}
+          onPress={logout}
+        >
           <Text style={styles.btnText}>Cerrar sesión</Text>
         </TouchableOpacity>
-    </View>
-       
-      
+      </View>
+
+
 
     </DrawerContentScrollView>
-   
+
   )
 }
 
-const styles=StyleSheet.create({
-  container:{
-    padding:15,
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
   },
-  imagen:{
-    width: 100, 
+  imagen: {
+    width: 100,
     height: 100,
-    borderRadius:100,
-    margin:20,
-    marginBottom:35
-   
+    borderRadius: 100,
+    margin: 20,
+    marginBottom: 35
+
   },
-  btnContainer:{
+  btnContainer: {
     /* backgroundColor:'red', */
     flexDirection: 'row',
-   /*  marginBottom:15,
-    padding:5 */
-    marginLeft:10,
-    marginBottom:10
-   
+    /*  marginBottom:15,
+     padding:5 */
+    marginLeft: 10,
+    marginBottom: 10
+
   },
-  btnText:{
+  btnText: {
     fontSize: 16,
     color: Colors.greenPet,
-    marginLeft:5,
-    marginTop:0,
-    
+    marginLeft: 5,
+    marginTop: 0,
+
   },
-  txtUser:{
+  txtUser: {
     fontSize: 16,
     color: Colors.greenPet,
-    marginLeft:5,
-    marginTop:0,
-    fontWeight:'bold'
-    
+    marginLeft: 5,
+    marginTop: 0,
+    fontWeight: 'bold'
+
   }
 })
